eec7827342fc3fcf50daa40f045d25350c3a4290
Matt Frawley <docfrawley@Matts-MBP-2.home>
2017-07-16 20:21:49 -0400

    add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,26 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+
+  db.collection('Users').deleteMany({name: 'Matt'}).then((result) =>{
+    console.log(result);
+  });
+  //deleteOne
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+    //   console.log(result);
+    // });
+
+  //findOneAndDelete
+  db.collection('Users').findOneAndDelete({
+    _id:new ObjectID('596b86ce848299039dc98efb')
+    }).then((result)=>{
+    console.log(result);
+  });
+  // db.close();
+});
