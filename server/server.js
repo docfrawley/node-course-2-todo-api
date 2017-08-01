3f1155ce076b90ff32543d45bb4c3b0ff531510e
Matt Frawley <docfrawley@Matts-MacBook-Pro-2.local>
2017-08-01 14:47:35 -0400

    test DELETE todos :id route

--- server/server.js
+++ server/server.js
@@ -56,15 +56,15 @@ app.delete('/todos/:id', (req, res) => {
     return res.status(404).send();
   }
 
-  Todo.findByIdAndRemove(id).then((todo)=>{
-    if (!todo){
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
       return res.status(404).send();
     }
-    res.send(todo);
-  }).catch((e) => {
-    res.status(400).send()
-  });
 
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
