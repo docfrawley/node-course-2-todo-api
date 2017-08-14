e7a434082fac49138f8ca56e914533b67e471e81
Matt Frawley <docfrawley@Matts-MBP-2.home>
2017-08-13 21:48:20 -0400

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -100,7 +100,6 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 //Post /users
-
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
@@ -116,6 +115,20 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+// POST /users/login {email, password}
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((e) => {
+    res.send(400).send();
+  });
 });
 
 app.listen(port, () => {
